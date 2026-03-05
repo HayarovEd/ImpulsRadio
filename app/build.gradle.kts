a5e0acc084ced56d3900b9237e27ac677555c730
Impuls <edurd@rambler.ru>
2026-03-05 15:17:47 +0500

    name radio and  composition - 2 lines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,13 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 3
-        versionName = "3.06"
+        versionName = "3.07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
+        setProperty("archivesBaseName", "Radio-$versionName")
     }
 
     buildTypes {
