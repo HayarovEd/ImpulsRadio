88bd26c5e7e101b94d572f9227b3f5871230c7e6
Impuls <edurd@rambler.ru>
2026-03-05 15:16:20 +0500

    name radio and  composition - 2 lines

--- app/src/main/java/com/edurda77/impuls/ui/main/MainScreen.kt
+++ app/src/main/java/com/edurda77/impuls/ui/main/MainScreen.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
